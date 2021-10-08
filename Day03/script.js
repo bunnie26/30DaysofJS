a4f09ecc8009876ad0005e63c2e875b5f0d07d90
Vinay Lakhani <lakhani.vnay.6377@gmail.com>
2021-10-08 11:20:41 +0530

    completed 3rd task

--- Day03/script.js
+++ Day03/script.js
@@ -2,11 +2,24 @@ const color = document.querySelector(".color")
 const space = document.querySelector(".space")
 const blur = document.querySelector(".blur")
 const inputs = document.querySelectorAll("input")
+const root = document.querySelector(":root")
 
-function handleUpdate(){
-    console.log(this.value)
-}
+// function handleUpdate(){
+//     console.log(this.value);
+// }
 
-inputs.forEach(input =>{
-    input.addEventListener('change',handleUpdate)
+// inputs.forEach(input =>{
+//     input.addEventListener('change',()=>{
+//         console.log(input.value);
+//     })
+// })
+
+color.addEventListener('input',()=>{
+    root.style.setProperty('--base',color.value)
+})
+space.addEventListener('input',()=>{
+    root.style.setProperty('--spacig',space.value+'px')
+})
+blur.addEventListener('input',()=>{
+    root.style.setProperty('--blur',blur.value+'px')
 })
